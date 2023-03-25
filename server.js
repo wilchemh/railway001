faf2a4e729577df8a0fee369fc7b2454560eca8e
Michael Wilchek <wilchemh@gmail.com>
2023-03-25 15:09:41 -0400

    todo step 01 - read static list

--- server.js
+++ server.js
@@ -2,8 +2,47 @@ const express = require('express');
 const app = express();
 const environment = require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello World 1!');
+// app.get('/', (req, res) => {
+//   res.send('Hello World 1!');
+// });
+
+
+var todos = [{
+    id: 1,
+    description: 'Eat Lunch',
+    compelted: false
+},{
+    id: 2,
+    description: 'Go to Store',
+    compelted: false
+}]
+
+app.get('/', function(req,res){
+    res.send('fbSTATS')
+});
+
+//GET /todos
+app.get('/api/todos', function(req,res){
+    res.json(todos);
+});
+
+//GET /todos/:id
+app.get('/api/todos/:id', function(req,res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo;
+
+    todos.forEach(function(todo){
+        if (todoId === todo.id) {
+            matchedTodo = todo
+        }
+    })
+    
+    if (matchedTodo){
+        res.json(matchedTodo);    
+    } else {
+        res.status(404).send();
+    }
+    
 });
 
 const server = app.listen(process.env.PORT || 8080, () => {
